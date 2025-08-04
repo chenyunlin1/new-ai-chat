7b7eb395c6d1c7af72a93a295d10445a87a0c145
陈运林 <102886365+chenyunlin1@users.noreply.github.com>
2025-08-04 09:31:19 +0800

    Update deepSeek.js

--- api/deepSeek.js
+++ api/deepSeek.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
     return res.status(204).end()
   }
-
+  console.log(111)
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
